caa5f738a24d53f7f4726a722a91f0742dba40ea
kirtanaramesh821-coder <kirtanaramesh821@gmail.com>
2026-05-16 18:16:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/kirtanaramesh821.github.io/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
